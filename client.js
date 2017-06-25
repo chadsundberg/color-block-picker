7d9837ced8e883faf874673f6050ebd0470903f5
Chad Sundberg <chadsundberg@gmail.com>
2017-06-25 08:49:03 -0500

    started game with six colors

--- client.js
+++ client.js
@@ -1,7 +1,7 @@
 // this array sets the starting colors and number of buttons
   var colorArray = ['red', 'green', 'yellow', 'blue', 'aqua', 'magenta', 'pink', 'white', 'purple'];
   var randomColor = '';
-  var numberOfColors = 4;
+  var numberOfColors = 6;
 
 $(document).ready(function(){
 
